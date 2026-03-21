3ef12d6118fc4fbfac5b8425cce753f187aad006
Antonioreyes8 <f2.arc.8@gmail.com>
2026-03-21 16:23:46 -0500

    line spacing and word spacing

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,16 +1,34 @@
 function About() {
-    return (
-        <section className="about-section">
-            <div className="information">
-                Denton, TX | (972) 746-6608 | 
-                <span>tony<span className="fallback">@</span>surco.studio</span> | 
-                25 years old | English & Spanish
-            </div>
-            <p className="about-text">
-                Hello, welcome to my personal portfolio.
-            </p>
-        </section>
-    );
+	return (
+		<section className="about-section">
+			<div className="information">
+				Denton, TX | (972) 746-6608 |
+				<span>
+					tony<span className="fallback">@</span>surco.studio
+				</span>{" "}
+				| 25 years old | English & Spanish
+			</div>
+			<p className="about-text">
+				Hello, welcome to my personal portfolio. I can work in both no-code and
+				code-based environments, and I have experience with a variety of tools
+				and technologies. I am passionate about creating beautiful and
+				functional websites.
+			</p>
+			<br />
+			<p className="about-text">
+				I've worked with Webflow, React, Tailwind, Python, Next.js. I usually
+				host all of my projects on Vercel, but I also have experience with
+				Netlify and AWS. I ususally use Supabase for my no-sql database needs
+				and I also have experience with mysql and postgresql.
+			</p>
+			<br />
+			<p className="about-text">
+				I am always looking for new opportunities to learn and grow as a
+				developer. If you have any questions or would like to work together,
+				please feel free to contact me.
+			</p>
+		</section>
+	);
 }
 
 export default About;
